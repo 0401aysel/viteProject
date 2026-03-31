999b38866e084f954b92fae4f57f5b4b61851f37
Aysel <aysel@gmail.com>
2026-03-31 12:10:02 +0400

    footer classNames optimized

--- components/footer/index.js
+++ components/footer/index.js
@@ -22,53 +22,67 @@ const Footer = `
                     </div>
                 </div>
                 <div class="footer-menu flex-2">
-                    <div class="footer-menu-flex flex flex-wrap justify-between">
+                    <div class="
+                        footer-menu-flex flex flex-wrap justify-between
+                        [&>div>h3]:md:text-2xl [&>div>h3]:text-lg [&>div>h3]:m-0 [&>div>h3]:font-thin [&>div>h3]:pr-[20px] [&>div>h3]:font-[PlayFairDisplay]
+
+                    ">
                         <div class="quicks">
-                            <h3 class="md:text-2xl text-lg m-0 font-thin pr-[20px] font-[PlayFairDisplay]">Quick Links</h3>
-                            <div class="menu-links py-[14px] md:py-[24px]">
-                                <a href="#" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">Services</a>
-                                <a href="#" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">Portfolio</a>
-                                <a href="/about.html" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">About us</a>
-                                <a href="#" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">Testimonial</a>
+                            <h3>Quick Links</h3>
+                            <div class="
+                                menu-links py-[14px] md:py-[24px]
+                                [&>a]:cursor-pointer [&>a]:text-(--white) [&>a]:block [&>a]:decoration-none [&>a]:text-sm [&>a]:leading-[28px]
+                            ">
+                                <a href="#">Services</a>
+                                <a href="#">Portfolio</a>
+                                <a href="/about.html">About us</a>
+                                <a href="#">Testimonial</a>
                             </div>
                         </div>
                         <div class="resources">
-                            <h3 class="md:text-2xl text-lg m-0 font-thin pr-[20px] font-[PlayFairDisplay]">Resources</h3>
-                            <div class="menu-links py-[14px] md:py-[24px]">
-                                <a href="#" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">Support</a>
-                                <a href="#" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">Privacy Policy</a>
-                                <a href="#" class="cursor-pointer text-(--white) block decoration-none text-sm leading-[28px]">Terms & Conditions</a>
+                            <h3>Resources</h3>
+                            <div class="
+                                menu-links py-[14px] md:py-[24px]
+                                [&>a]:cursor-pointer [&>a]:text-(--white) [&>a]:block [&>a]:decoration-none [&>a]:text-sm [&>a]:leading-[28px]
+                            ">
+                                <a href="#">Support</a>
+                                <a href="#">Privacy Policy</a>
+                                <a href="#">Terms & Conditions</a>
                             </div>
                         </div>
                         <div class="social">
-                            <h3 class="md:text-2xl text-lg m-0 font-thin pr-[20px] font-[PlayFairDisplay]">Social Media</h3>
-                            <div class="social-links py-[24px] opacity-70">
-                                <a href="#" class="cursor-pointer facebook px-[10px] py-[7px] inline-block bg-white/8 decoration-none">
-                                    <img class="w-[18px]" src="/images/facebook.svg">
+                            <h3>Social Media</h3>
+                            <div class="
+                                social-links py-[24px] opacity-70 
+                                [&>a>img]:w-[18px] 
+                                [&>a]:inline-block [&>a]:bg-white/8 [&>a]:decoration-none [&>a]:px-[10px] [&>a]:py-[7px] [&>a]:cursor-pointer
+                            ">
+                                <a href="#">
+                                    <img src="/images/facebook.svg">
                                 </a>
-                                <a href="#" class="cursor-pointer twitter px-[10px] py-[7px] inline-block bg-white/8 decoration-none">
-                                    <img class="w-[18px]" src="/images/twitter.svg">
+                                <a href="#">
+                                    <img src="/images/twitter.svg">
                                 </a>
-                                <a href="#" class="cursor-pointer instagram px-[10px] py-[7px] inline-block bg-white/8 decoration-none">
-                                    <img class="w-[18px]" src="/images/instagram.svg">
+                                <a href="#">
+                                    <img src="/images/instagram.svg">
                                 </a>
-                                <a href="#" class="cursor-pointer linkedin px-[10px] py-[7px] inline-block bg-white/8 decoration-none">
-                                    <img class="w-[18px]" src="/images/linkedin.svg">
+                                <a href="#">
+                                    <img src="/images/linkedin.svg">
                                 </a>
-                                <a href="#" class="cursor-pointer youtube px-[10px] py-[7px] inline-block bg-white/8 decoration-none">
-                                    <img class="w-[18px]" src="/images/youtube.svg">
+                                <a href="#">
+                                    <img src="/images/youtube.svg">
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div class="subscribe">
-                        <h3 class="md:text-2xl text-lg m-0 font-thin pr-[20px] font-[PlayFairDisplay]">Subscribe</h3>
+                        <h3>Subscribe</h3>
                         <form class="subscribe-form relative mt-[10px] md:mt-[24px]">
                             <label for="mail" class="absolute w-max block md:top-[18px] md:left-[20px] top-[14px] left-[13px]">
                                 <img class="md:w-[24px] w-[18px]" src="/images/mail.png">
                             </label>
                             <input  id ="mail" class="transition-all duration-200 ease-linear subscibe-mail md:h-[59px] h-[46px] pl-[40px] md:pl-[50px] text-(--white) bg-white/12 rounded-[16px] w-full box-border placeholder:opacity-0 md:placeholder:opacity-100 placeholder:text-base placeholder:font-[Poppins] placeholder:text-white/40" name="mail" placeholder="name@domain.com"/>
-                            <button type="submit" class="active:scale-90 transition-all duration-200 ease-linear cursor-pointer absolute font-[Poppins] md:top-[17px] top-[6px] right-[6px] md:right-[18px]">Send</button>
+                            <button type="submit" class="active:scale-90 transition-all duration-200 ease-linear cursor-pointer absolute font-[Poppins] md:top-[17px] top-[10px] right-[16px] md:right-[18px]">Send</button>
                             <p class="mail-error text-white text-xs"></p>
                         </form>
                     </div>
